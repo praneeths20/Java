15831d8b00e71bea8e96e7858c909d7e3deb7441
Gaurav <Gaurav@LAPTOP-3QOCMFPH>
2022-03-13 20:31:05 +0530

    Work done -SP1

---
+++ src/test/java/test/StringReplace.java
@@ -0,0 +1,12 @@
+package test;
+
+public class StringReplace
+{
+	public static void main(String[] args) 
+	 {
+		
+      String x = "lafkdjldsjlks348klkjk39";
+      String y= x.replaceAll("[^0-9]"," ");
+      System.out.println(y);
+     }
+}
